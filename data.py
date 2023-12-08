b1b5f3d03edbe79bb081d6860134e66c8aff9a23
Dwi Didit Prasetiyo <didit@dwidi.com>
2023-12-08 22:31:15 +0700

    Add main functionality with supplemental data and Pipfile
    The commit includes the addition of a new Pipfile, a gitignore file, and two Python scripts. The main script contains a function that calculates the score of word inputs based on a predefined point system. The data script includes the points for each letter and a set of predefined words. The gitignore file was added to ignore certain files and directories in the .idea directory.

---
+++ data.py
@@ -0,0 +1,9 @@
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+player_to_words = {
+    "player1": ["BLUE", "TENNIS", "EXIT"],
+    "wordNerd": ["EARTH", "EYES", "MACHINE"],
+    "Lexi Con": ["ERASER", "BELLY", "HUSKY"],
+    "Prof Reader": ["ZAP", "COMA", "PERIOD"]
+}
---
+++ main.py
@@ -0,0 +1,31 @@
+from data import letters, points, player_to_words
+
+letter_to_points = zip(letters, points)
+
+letter_to_points = {letters:points for letters, points in letter_to_points}
+
+letter_to_points[" "] = 0
+
+def score_word(word):
+    point_total = 0
+    for letter in word:
+        point_total += letter_to_points.get(letter.upper(), 0)
+    return point_total
+
+brownie_points = score_word("brownie")
+
+player_to_points = {}
+
+# Iterate in player_to_words
+for player, words in player_to_words.items():
+    player_points = 0
+    for word in words:
+        for letter in word:
+            player_points += letter_to_points.get(letter.upper(), 0)
+    player_to_points[player] = player_points
+print(player_to_points)
+
+
+
+
+
